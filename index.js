648f19d57fd302feb12c58a463d9267fa6153e1a
EXPEakhasawneh <akhasawneh@expedia.com>
2019-03-17 14:22:19 +0200

    Second commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function initialCell() {
     return [1, [0, 0]]
 }
 
+function initialiseCell(point) {
+    return [0, point];
+}
+
 function pointShape(point) {
     return checkTuple(point) && checkTuple(point[1]);
 }
@@ -14,9 +18,44 @@ function getStateOfCell(point) {
     return point[0];
 }
 
+function isEqual(point1Coordinations, point2Coordinations) {
+    return point1Coordinations[0] === point2Coordinations[0] && point1Coordinations[1] === point2Coordinations[1];
+}
+
+function findCellInLifeList(pointCoordinations, lifeList) {
+    return lifeList.find(function (Cell) {
+        return isEqual(pointCoordinations, Cell[1]);
+    }) || false;
+}
+
+function neighborsList(pointCoordinations) {
+    const neighbors = [];
+    let xAxisPoint = pointCoordinations[1][0];
+    let yAxisPoint = pointCoordinations[1][1];
+    for (let i = xAxisPoint - 1; i <= xAxisPoint+1; i++) {
+        for (let j = yAxisPoint - 1; j <= yAxisPoint+1; j++) {
+            const neighborPointCoordiniations = [i, j];
+            if (isEqual(pointCoordinations[1], neighborPointCoordiniations)) {
+                continue;
+            }
+            if (!findCellInLifeList(neighborPointCoordiniations, lifeList)) {
+                lifeList.push(initialiseCell(neighborPointCoordiniations));
+            }
+            neighbors.push(findCellInLifeList(neighborPointCoordiniations, lifeList));
+        }
+    }
+    return neighbors;
+}
+
+const lifeList = [];
+
 module.exports = {
     initialCell,
     pointShape,
     checkTuple,
     getStateOfCell,
+    isEqual,
+    initialiseCell,
+    findCellInLifeList,
+    neighborsList,
 };
--- test/index.js
+++ test/index.js
@@ -1,4 +1,7 @@
-const assert = require('chai').assert;
+const chai = require('chai');
+const assertArrays = require('chai-arrays');
+const { assert } = chai;
+chai.use(assertArrays);
 const main = require('../index');
 
 describe('Game of Life suite', function() {
@@ -12,15 +15,57 @@ describe('Game of Life suite', function() {
         assert.deepEqual(main.pointShape(main.initialCell()), true);
     });
 
-    it('return true for tuple', function () {
+    it('should return true for tuple', function () {
         assert.deepEqual(main.checkTuple([0, 0]), true);
     });
 
-    it('return false for tuple', function () {
-       assert.deepEqual(main.checkTuple({tuple: [0, 0, 0]}), false);
+    it('should return false for tuple', function () {
+        assert.deepEqual(main.checkTuple({tuple: [0, 0, 0]}), false);
     });
 
-    it('return the neighbor state', function () {
-        assert.deepEqual(main.getStateOfCell([1, [0, 0+1]]), 1);
+    it('should return state of Cell', function () {
+        assert.deepEqual(main.getStateOfCell([1, [0, 0]]), 1);
     });
+
+    it('should compare two points, return true if they are the same point', function () {
+        assert.deepEqual(main.isEqual([0, 0], [0, 0]), true);
+    });
+
+    it('should compare two points, return false if they are not the same point', function () {
+        assert.deepEqual(main.isEqual([1, 2], [1, 1]), false);
+    });
+    
+    it('should initialise point with state equal 0 if the point is not exist', function () {
+        assert.deepEqual(main.initialiseCell([1, 1]), [0, [1, 1]]);
+    });
+    
+    it('should return Cell if it exists in the lifeList', function () {
+        assert.deepEqual(main.findCellInLifeList([1, 1], [main.initialCell(), [0, [1, 1]]]), [0, [1, 1]]);
+    });
+
+    it('should false if Cell does not exist in the lifeList', function () {
+        assert.deepEqual(main.findCellInLifeList([1, 1], [main.initialCell()]), false);
+    });
+    
+    it('return list of neighbors for a given point', function () {
+        /*
+            p1, p2,         p3 
+            p4, givenPoint, p5
+            p6, p7,         p8
+        */
+        const expected = [
+            [0, [-1, 1]], [0, [0, 1]], [0, [1, 1]],
+            [0, [-1, 0]], [0, [1, 0]],
+            [0, [-1, -1]], [0, [0, -1]], [0, [1, -1]]
+        ];
+        
+        expected.map(Cell =>{
+            assert.deepInclude(main.neighborsList(main.initialCell()), Cell);
+        });
+    });
+    
+    it('check for underpopulation condition, if a living cell has less than two living neighbors', function() {
+        const point = main.neighborsList(main.initialCell());
+        assert.equal(main.checkUnderpopulation(point), true);
+    })
 });
